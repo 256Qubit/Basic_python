354d68d746629bcf739734b7fcc318c1b363c476
256Qubit <jmortyrick@gmail.com>
2024-01-11 10:20:19 +0800

    day 06

--- Day6/Test1.py
+++ Day6/Test1.py
@@ -7,6 +7,7 @@ print(name)
 name.pop(0)
 print(name)
 
+# 清楚列表，清楚后列表仍旧存在s
 name.clear()
 print(name)
 
@@ -14,4 +15,15 @@ name.extend(["李一", "李二", "李三", "李四"])
 print(name)
 
 del name
+# 删除列表，删除后列表已经不存在
+
+
+# 数字列表排序,默认是升序
+numbers = [55, 44, 465, 653, 42, 75, 13, 11]
+numbers.sort()
+print(numbers)
+
+# 添加reverse=True,降序排序
+numbers.sort(reverse=True)
+print(numbers)
 
